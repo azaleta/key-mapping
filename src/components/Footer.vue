a0c0e715af3d3379c9d37b72bd2397b6e72ff94c
azaleta <24407500@qq.com>
2022-06-20 13:20:50 +0800

    feat: fix dark switch icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,8 +5,7 @@
 <template>
   <nav text-xl mt-6 inline-flex gap-2>
     <button class="icon-btn !outline-none" @click="toggleDark()">
-      <div v-if="isDark" i-carbon-moon />
-      <div v-else i-carbon-sun />
+      <div i-carbon-moon dark:i-carbon-sun />
     </button>
 
     <a
